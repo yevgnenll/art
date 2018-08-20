b19ff8d54a8a25c3c3b84bebe79b791f6bbc080f
seungkwon <yevgnenll@gmail.com>
2018-08-20 23:36:48 +0900

    artist, image domain

--- src/main/java/com/yevgnenll/artist/domain/Artist.java
+++ src/main/java/com/yevgnenll/artist/domain/Artist.java
@@ -1,4 +1,33 @@
 package com.yevgnenll.artist.domain;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+
+@Data
 public class Artist {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(length = 45, nullable = false)
+  private String name;
+
+  private Date birthYear;
+
+  private Date deathYear;
+
+  @Column(length = 45)
+  private String country;
+
+  @Column(length = 45)
+  private String genre;
+
 }
--- src/main/java/com/yevgnenll/artist/domain/Image.java
+++ src/main/java/com/yevgnenll/artist/domain/Image.java
@@ -1,4 +1,27 @@
 package com.yevgnenll.artist.domain;
 
+import java.net.URL;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+
+@Data
 public class Image {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private URL imageUrl;
+
+  private String title;
+
+  private long artistId;
+
+  private String description;
+
 }
