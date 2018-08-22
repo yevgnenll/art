a53dee67fe819f1a2ebaaa516e37696528900c8d
seungkwon <yevgnenll@gmail.com>
2018-08-22 21:14:33 +0900

    image repository를 구현합니다

---
+++ src/main/java/com/yevgnenll/artist/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package com.yevgnenll.artist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.yevgnenll.artist.domain.Image;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long>{
+}
