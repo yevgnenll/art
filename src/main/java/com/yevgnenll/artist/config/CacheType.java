b7ed07f0c230ab48ca44ed311956ba2eb78ccd2f
seungkwon <yevgnenll@gmail.com>
2018-08-26 01:54:37 +0900

    cache 설정
    두 개의 API는 요구사항이 다르다
    예술가 리스트를 제공하는 API는 5분마다 캐시가 evict 되어야 하고
    하나의 예술가의 상세정보를 제공하는 API는 24시간 뒤에 evict를 한다

---
+++ src/main/java/com/yevgnenll/artist/config/CacheType.java
@@ -0,0 +1,21 @@
+package com.yevgnenll.artist.config;
+
+import lombok.Getter;
+
+@Getter
+public enum CacheType {
+
+  ARTISTS("artists", 5 * 60, 10000),
+  ARTIST_INFO("artistInfo", 24 * 60 * 60, 10000);
+
+  CacheType(String cacheName, int expiredAfterWrite, int maximumSize) {
+    this.cacheName = cacheName;
+    this.expiredAfterWrite = expiredAfterWrite;
+    this.maximumSize = maximumSize;
+  }
+
+  private String cacheName;
+  private int expiredAfterWrite;
+  private int maximumSize;
+
+}
