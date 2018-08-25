b970c8437ad13d960c7843feac02580f739e87b3
seungkwon <yevgnenll@gmail.com>
2018-08-26 01:26:19 +0900

    cache를 사용하는 것으로 정의합니다

--- src/main/java/com/yevgnenll/artist/ArtistApplication.java
+++ src/main/java/com/yevgnenll/artist/ArtistApplication.java
@@ -2,7 +2,9 @@ package com.yevgnenll.artist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class ArtistApplication {
 
